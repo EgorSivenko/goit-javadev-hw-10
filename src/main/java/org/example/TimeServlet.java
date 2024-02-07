22f23db48a9860ab480b640be79685e80b115b81
EgorSivenko <egor.siv20@gmail.com>
2024-02-07 14:31:15 +0200

    TimeServlet code cleanup

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
@@ -20,11 +19,7 @@ public class TimeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String timezone = req.getParameter("timezone");
 
-        if (Objects.isNull(timezone)) {
-            timezone = DEFAULT_TIMEZONE;
-        }
-
-        ZoneId zoneId = ZoneId.of(timezone);
+        ZoneId zoneId = ZoneId.of(timezone != null ? timezone : DEFAULT_TIMEZONE);
         ZonedDateTime now = ZonedDateTime.now(zoneId);
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
